a099e61d30c625320d6c604f77a25445975e79a5
SamAnnozaili <sam456eg@gmail.com>
2025-09-17 16:36:18 +0300

    test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -74,7 +74,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(81, 140);
+            this.label2.Location = new System.Drawing.Point(85, 140);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(132, 24);
             this.label2.TabIndex = 1;
